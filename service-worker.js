35edcc982ae0f470a57326318ac5e8ae4b98e728
drchrisslininger <DrSlininger@CerebralChiropractic.com>
2026-07-02 23:18:30 -0400

    Re-deploy: publish quantity dropdown to Pages (SW v6)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Cerebral Staff Dashboard - offline app shell */
-const CACHE = 'ccc-staff-v5';
+const CACHE = 'ccc-staff-v6';
 const ASSETS = [
   './', './index.html', './logo.png', './manifest.webmanifest',
   './icon-192.png', './icon-512.png'
